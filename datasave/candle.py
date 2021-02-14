bde2b0e328d2336ba953e15a6467ad305ef17987
sesang06 <sesang06@naver.com>
2021-02-14 21:35:09 +0900

    백테스팅 구현

--- datasave/candle.py
+++ datasave/candle.py
@@ -75,44 +75,20 @@ def RSI(array, n):
     rs = gain.ewm(n).mean() / loss.abs().ewm(n).mean()
     return 100 - 100 / (1 + rs)
 class SmaCross(Strategy):
-    d_rsi = 30  # Daily RSI lookback periods
-    w_rsi = 30  # Weekly
-    level = 70
-
     def init(self):
-        # Compute moving averages the strategy demands
-        self.ma10 = self.I(SMA, self.data.Close, 10)
-        self.ma20 = self.I(SMA, self.data.Close, 20)
-        self.ma50 = self.I(SMA, self.data.Close, 50)
-        self.ma100 = self.I(SMA, self.data.Close, 100)
-
-        # Compute daily RSI(30)
-        self.daily_rsi = self.I(RSI, self.data.Close, self.d_rsi)
-
-        # To construct weekly RSI, we can use `resample_apply()`
-        # helper function from the library
-        self.weekly_rsi = resample_apply(
-            'W-FRI', RSI, self.data.Close, self.w_rsi)
+        price = self.data.Close
+        self.ma1 = self.I(SMA, price, 10)
+        self.ma2 = self.I(SMA, price, 20)
 
     def next(self):
-        price = self.data.Close[-1]
-
-        # If we don't already have a position, and
-        # if all conditions are satisfied, enter long.
-        if (not self.position and
-                self.daily_rsi[-1] > self.level and
-                self.weekly_rsi[-1] > self.level and
-                self.weekly_rsi[-1] > self.daily_rsi[-1] and
-                self.ma100[-1] < self.ma50[-1] < self.ma20[-1] < self.ma10[-1] < price):
-
-            # Buy at market price on next open, but do
-            # set 8% fixed stop loss.
-            self.buy(sl=.92 * price)
-
-        # If the price closes 2% or more below 10-day MA
-        # close the position, if any.
-        elif price < .98 * self.ma10[-1]:
-            self.position.close()
+        if crossover(self.ma1, self.ma2):
+            # if self.position.is_short:
+            #     self.()
+            self.buy()
+        elif crossover(self.ma2, self.ma1):
+            # if self.position.is_long:
+            #     self.position.close()
+            self.sell()
 
 def executeBacktest(data: pandas.DataFrame):
     data = data.set_index('openTime')
@@ -122,7 +98,7 @@ def executeBacktest(data: pandas.DataFrame):
     data['Close'] = data['close']
     data['Volume'] = data['volume']
     bt = Backtest(data, SmaCross,
-                  cash=100000000000, commission=.002)
+                  cash=100000000000, commission=.002, exclusive_orders=True)
 
     print(bt.run())
     bt.plot()
