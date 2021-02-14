155454165dfab714afc8ccf201dc2b9d2743f066
sesang06 <sesang06@naver.com>
2021-02-14 19:20:30 +0900

    임시저장

--- datasave/candle.py
+++ datasave/candle.py
@@ -51,10 +51,85 @@ def printDataAsGraph(df: pandas.DataFrame):
     axes[1].set_xticklabels(_xlabels, rotation=45, minor=False)
     plt.tight_layout()
     plt.show()
+from backtesting import Backtest, Strategy
+from backtesting.lib import crossover
+
+from backtesting.test import SMA, GOOG
+from backtesting import Strategy, Backtest
+from backtesting.lib import resample_apply
+import pandas as pd
+
+
+def SMA(array, n):
+    """Simple moving average"""
+    return pd.Series(array).rolling(n).mean()
+
+
+def RSI(array, n):
+    """Relative strength index"""
+    # Approximate; good enough
+    gain = pd.Series(array).diff()
+    loss = gain.copy()
+    gain[gain < 0] = 0
+    loss[loss > 0] = 0
+    rs = gain.ewm(n).mean() / loss.abs().ewm(n).mean()
+    return 100 - 100 / (1 + rs)
+class SmaCross(Strategy):
+    d_rsi = 30  # Daily RSI lookback periods
+    w_rsi = 30  # Weekly
+    level = 70
+
+    def init(self):
+        # Compute moving averages the strategy demands
+        self.ma10 = self.I(SMA, self.data.Close, 10)
+        self.ma20 = self.I(SMA, self.data.Close, 20)
+        self.ma50 = self.I(SMA, self.data.Close, 50)
+        self.ma100 = self.I(SMA, self.data.Close, 100)
+
+        # Compute daily RSI(30)
+        self.daily_rsi = self.I(RSI, self.data.Close, self.d_rsi)
+
+        # To construct weekly RSI, we can use `resample_apply()`
+        # helper function from the library
+        self.weekly_rsi = resample_apply(
+            'W-FRI', RSI, self.data.Close, self.w_rsi)
+
+    def next(self):
+        price = self.data.Close[-1]
+
+        # If we don't already have a position, and
+        # if all conditions are satisfied, enter long.
+        if (not self.position and
+                self.daily_rsi[-1] > self.level and
+                self.weekly_rsi[-1] > self.level and
+                self.weekly_rsi[-1] > self.daily_rsi[-1] and
+                self.ma100[-1] < self.ma50[-1] < self.ma20[-1] < self.ma10[-1] < price):
+
+            # Buy at market price on next open, but do
+            # set 8% fixed stop loss.
+            self.buy(sl=.92 * price)
+
+        # If the price closes 2% or more below 10-day MA
+        # close the position, if any.
+        elif price < .98 * self.ma10[-1]:
+            self.position.close()
+
+def executeBacktest(data: pandas.DataFrame):
+    data = data.set_index('openTime')
+    data['Open'] = data['open']
+    data['High'] = data['high']
+    data['Low'] = data['low']
+    data['Close'] = data['close']
+    data['Volume'] = data['volume']
+    bt = Backtest(data, SmaCross,
+                  cash=100000000000, commission=.002)
+
+    print(bt.run())
+    bt.plot()
 def printCandle():
     request_client = RequestClient(api_key=g_api_key, secret_key=g_secret_key)
 
-    result: [Candlestick] = request_client.get_candlestick_data(symbol="BTCUSDT", interval=CandlestickInterval.DAY1,
+    result: [Candlestick] = request_client.get_candlestick_data(symbol="BTCUSDT", interval=CandlestickInterval.MIN1,
                                       startTime=None, endTime=None, limit=500)
     # np = numpy.vectorize(result)
     # print(np)
@@ -87,7 +162,8 @@ def printCandle():
     print(df)
     df.to_excel("output.xlsx")
 
-    printDataAsGraph(df.tail(30))
+    printDataAsGraph(df.tail(90))
+    executeBacktest(df)
 
 
 if __name__ == '__main__':
