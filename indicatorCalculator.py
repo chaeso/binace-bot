0334cb41901f81aa6d473d3ab8a3fa50ef4db5c4
sesang06 <sesang06@naver.com>
2021-02-15 17:10:17 +0900

    지표 값 작성

--- indicatorCalculator.py
+++ indicatorCalculator.py
@@ -1,15 +1,76 @@
-class IndicatorCalculator:
+from abc import *
+import pandas as pd
+import talib
 
-    def SMA(self, array, n):
-        """Simple moving average"""
-        return pd.Series(array).rolling(n).mean()
+class BaseIndicatorBuilder(metaclass=ABCMeta):
+    """
+       지표들을 만드는 베이스 클래스.
+    """
+    _name: str
 
-    def RSI(array, n):
-        """Relative strength index"""
-        # Approximate; good enough
-        gain = pd.Series(array).diff()
-        loss = gain.copy()
-        gain[gain < 0] = 0
-        loss[loss > 0] = 0
-        rs = gain.ewm(n).mean() / loss.abs().ewm(n).mean()
-        return 100 - 100 / (1 + rs)
+    def __init__(self, name: str):
+        self._name = name
+
+    @property
+    def name(self) -> str:
+        """
+        지표들의 이름을 리턴한다.
+        :return: 지표명
+        """
+        return self._name
+
+    @abstractmethod
+    def build_indicator(self, df: pd.DataFrame):
+        """
+        데이터를 바탕으로 지표를 빌드하낟.
+        :param df: 데티어
+        :return: void
+        """
+        pass
+
+
+class RSIBuilder(BaseIndicatorBuilder):
+    """
+        RSI를 계산해 반환한다.
+    """
+    _days: int
+
+    def __init__(self, days: int):
+        name: str = "RSI" + str(days)
+        super.__init__(self, name=name)
+        self._days = days
+
+    def build_indicator(self, df: pd.DataFrame):
+        close = df['close']
+        df[self.name] = talib.RSI(close, self._days)
+
+class SMABuilder(BaseIndicatorBuilder):
+    """
+    SMA를 계산해 반환한다.
+    """
+    _days: int
+
+    def __init__(self, days: int):
+        name: str = "SMA" + str(days)
+        super.__init__(self, name=name)
+        self._days = days
+
+    def build_indicator(self, df: pd.DataFrame):
+        close = df['close']
+        df[self.name] = talib.SMA(close, self._days)
+
+
+class EMABuilder(BaseIndicatorBuilder):
+    """
+    EMA를 계산해 반환한다.
+    """
+    _days: int
+
+    def __init__(self, days: int):
+        name: str = "EMA" + str(days)
+        super.__init__(self, name=name)
+        self._days = days
+
+    def build_indicator(self, df: pd.DataFrame):
+        close = df['close']
+        df[self.name] = talib.EMA(close, self._days)
